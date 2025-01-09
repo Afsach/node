9bf3a88ede100531c1aa256339037567d41b08f3
Afsach <nagma1804@gmail.com>
2025-01-09 18:55:08 +0530

    exporting Fruits directory to script.js using export.modoule from index.js file

---
+++ Fruits/grapes.js
@@ -0,0 +1,4 @@
+module.exports = {
+    name : 'grapes',
+    color : 'green'
+}
---
+++ Fruits/index.js
@@ -0,0 +1,8 @@
+const apple = require('./apple');
+const banana = require('./banana');
+const grapes = require('./grapes');
+const orange = require('./orange');
+
+
+let fruits = [apple, orange, grapes, banana];
+module.exports = fruits;
